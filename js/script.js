fe179331a2e234da6d3f43ecab28bb8a7317a3a4
kriskera <karkerakrishna10@gmail.com>
2023-03-25 00:32:29 +0530

    First upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// function myFunction() {
+//   var x = document.querySelector("#nav-menu");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
+
+
+function myFunction() {
+  var x = document.querySelector("#nav-menu");
+  var y = document.querySelector(".navbar");
+  if (x.style.height === "0px") {
+    x.style.visibility = "visible";
+    x.style.opacity = "1";
+    x.style.height = "90%";
+    y.style.height = "100%";
+    
+  } else {
+    x.style.visibility = "hidden";
+    x.style.opacity = "0";
+    x.style.height = "0";
+    y.style.height = "auto";
+  }
+}
