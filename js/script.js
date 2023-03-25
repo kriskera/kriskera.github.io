07a09fcfd3ea91f097ae7b0f6ef4659b6411686b
kriskera <karkerakrishna10@gmail.com>
2023-03-26 00:18:58 +0530

    Digital animation completed

--- js/script.js
+++ js/script.js
@@ -25,3 +25,49 @@ function myFunction() {
     y.style.height = "auto";
   }
 }
+
+// let el = document.querySelector(".bit-color-shift");
+// function shiftColor() {
+//   let myElements = document.querySelector(".bit-color-shift").children;
+
+//   const color = {
+//     red: "#FF2C49",
+//     orange: "#FF6D46",
+//     yellow: "#FFE751",
+//     green: "#2FF630",
+//     blue: "#00AAF8",
+//     purple: "#7D5EF7",
+//     darkGreen: "#00D574"
+//   };
+
+//   let hexCode = Object.values(color);
+  
+//   let count = 0;
+
+//   function cycleArray() {
+//     let myElement = myElements[count];
+//     console.log(myElement);
+//     let colour = hexCode[count];
+//     console.log("Colour: "+ colour)
+//     myElement.style.color = colour;
+
+//     // increment our counter
+//     count++;
+//     console.log(count);
+
+//     // reset counter if we reach end of array
+//     if (count === myElements.length) {
+//       count = 0;
+//     }
+//   }
+
+// setInterval(cycleArray, 500);
+// };
+
+
+
+
+
+
+
+
