120e5e055730a3656f386249ab68885609ad74b0
kriskera <karkerakrishna10@gmail.com>
2023-04-14 16:47:48 +0530

    loading screen

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-// function myFunction() {
-//   var x = document.querySelector("#nav-menu");
-//   if (x.style.display === "block") {
-//     x.style.display = "none";
-//   } else {
-//     x.style.display = "block";
-//   }
-// }
-
-
 
 function myFunction() {
   var x = document.querySelector("#nav-menu");
@@ -26,45 +16,10 @@ function myFunction() {
   }
 }
 
-// let el = document.querySelector(".bit-color-shift");
-// function shiftColor() {
-//   let myElements = document.querySelector(".bit-color-shift").children;
-
-//   const color = {
-//     red: "#FF2C49",
-//     orange: "#FF6D46",
-//     yellow: "#FFE751",
-//     green: "#2FF630",
-//     blue: "#00AAF8",
-//     purple: "#7D5EF7",
-//     darkGreen: "#00D574"
-//   };
-
-//   let hexCode = Object.values(color);
-  
-//   let count = 0;
-
-//   function cycleArray() {
-//     let myElement = myElements[count];
-//     console.log(myElement);
-//     let colour = hexCode[count];
-//     console.log("Colour: "+ colour)
-//     myElement.style.color = colour;
-
-//     // increment our counter
-//     count++;
-//     console.log(count);
-
-//     // reset counter if we reach end of array
-//     if (count === myElements.length) {
-//       count = 0;
-//     }
-//   }
-
-// setInterval(cycleArray, 500);
-// };
-
-
+//disable right click
+// document.addEventListener("contextmenu", function (e){
+//   e.preventDefault();
+// }, false);
 
 
 
