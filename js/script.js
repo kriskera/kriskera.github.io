027fd7d9a771176bd8901750c9a722cfa257dbdf
kriskera <karkerakrishna10@gmail.com>
2023-04-15 13:57:05 +0530

    Privacy Policy page

--- js/script.js
+++ js/script.js
@@ -5,15 +5,14 @@ function myFunction() {
   if (x.style.height === "0px") {
     x.style.visibility = "visible";
     x.style.opacity = "1";
-    x.style.height = "90%";
+    x.style.height = "80%";
     y.style.height = "100%";
     
   } else {
     x.style.visibility = "hidden";
     x.style.opacity = "0";
     x.style.height = "0";
-    x.style.width = "0";
-    y.style.height = "auto";
+    y.style.height = "0";
   }
 }
 
